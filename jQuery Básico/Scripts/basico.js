44461693950f390f3b89f0cb3cf836b284133298
alvaro <alraled@hotmail.es>
2016-02-23 09:16:13 +0100

    jQuery_Básico

---
+++ jQuery Básico/Scripts/basico.js
@@ -0,0 +1,24 @@
+﻿function ocultardivs() {
+
+    $("div").html("Empieza el mareo");
+    $("div").hide(2000);
+}
+
+function mostrardivs() {
+    $("divs").show(3000, ocultardivs);
+}
+
+$(document).ready(function() {
+    alert("He cargado");
+    $("#uno").click(ocultardivs);
+    $("button[type=button]").click(
+        function() {
+            e.preventDefault();
+            $("div").show();
+        }
+    );
+});
+
+//EL ATRIBUTO VAL SE USA PARA LOS INPUT//
+// EL ATRIBUTO TEXT NO SE USA PARA LOS INPUT//
+// EL ATRIBUTO HTML ESCRIBE TEXTO PLANO UNICAMENTE //
